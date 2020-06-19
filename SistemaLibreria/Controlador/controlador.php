5507d700701d3a8b10244568cc861bde1bafa2f7
Acer <Acer@DESKTOP-8K7ECJL>
2020-06-18 20:11:17 -0500

    se crea controlador de login

--- SistemaLibreria/Controlador/controlador.php
+++ SistemaLibreria/Controlador/controlador.php
@@ -8,7 +8,7 @@ if ($_GET) {
 //    $datos = ['id'=>$_GET['cod']];
 //    $sql="update usuarios set estado ='I' where cod=:id";
     $sql = "update usuarios set estado ='E' where codigo=$cod";
-    $sqlQuery = $pdo45->prepare($sql);
+    $sqlQuery = $pdo->prepare($sql);
     $sqlQuery->execute();
 //  echo "<script>bootbox.confirm('Usuario Eliminado',funcion);</script>";
     // echo "<script>alert('Usuario Eliminado');location.href='../usuario.php'</script>";
@@ -17,9 +17,9 @@ if ($_GET) {
 }
 //*************************************METODO INSERTAR********************************************
 if (isset($_POST['btnguardar']) != null) {
-    $datos = ['nombre' => $_POST['txtnombres'], 'usuario' => $_POST['txtcorreo'], 'password' => $_POST['txtpassword'], 'rol' => $_POST['cbrol']];
-    $sql = "insert into usuarios (nombre,usuario,password,rol,estado) values(:nombre,:usuario,:password,:rol,'A')";
-    $sqlQuery = $pdo45->prepare($sql);
+    $datos = ['nombre' => $_POST['txtnombres'], 'usuario' => $_POST['txtcorreo'], 'contraseña' => $_POST['txtpassword'], 'rol' => $_POST['cbrol']];
+    $sql = "insert into usuarios (nombre,usuario,password,rol,estado) values(:nombre,:usuario,:contraseña,:rol,'A')";
+    $sqlQuery = $pdo->prepare($sql);
     $sqlQuery->execute($datos);
     //$_SESSION['UsuarioRegistrado'] = 'OK';
     header("Location:../Usuarios.php");
@@ -28,9 +28,9 @@ if (isset($_POST['btnguardar']) != null) {
 //*************************************METODO MODIFICAR********************************************
 if (isset($_POST['btnmodificar']) != null) {
     $codid=$_POST['txtcod'];
-    $datos = ['nombre' => $_POST['txtnombres'], 'usuario' => $_POST['txtcorreo'], 'password' => $_POST['txtpassword'], 'rol' => $_POST['cbrol']];
+    $datos = ['nombre' => $_POST['txtnombres'], 'usuario' => $_POST['txtcorreo'], 'contraseña' => $_POST['txtpassword'], 'rol' => $_POST['cbrol']];
     $sql = "update usuarios set nombre=:nombre,usuario=:usuario,password=:password,rol=:rol where codigo=$codid";
-    $sqlQuery = $pdo45->prepare($sql);
+    $sqlQuery = $pdo->prepare($sql);
     $sqlQuery->execute($datos);
     //$_SESSION['UsuarioRegistrado'] = 'OK';
     header("Location:../Usuarios.php");
