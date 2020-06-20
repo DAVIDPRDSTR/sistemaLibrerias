dc6ca4a3e99b4af29ac058f5fe7811fcbce49573
Wolverine <Wolverine@WOLVERINE>
2020-06-20 14:09:33 -0500

    Cambio en la conexión a base de datos.

--- SistemaLibreria/Datos/conexion.php
+++ SistemaLibreria/Datos/conexion.php
@@ -1,6 +1,7 @@
 <?php
 try {
-    $pdo=new PDO("mysql:dbname=sistemalibreria;host=localhost","root","");
+    $pdo=new PDO("mysql:dbname=sql12349814;host=sql12.freemysqlhosting.net","sql12349814","i4g74Wy3ij");
+
     
     
 } catch (Exception $ex) {
